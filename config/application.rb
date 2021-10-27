dfaeca5d944346694570b3edfc05de7e3def30fd
Hitoshi-unten <untenht@gmail.com>
2021-10-28 00:29:21 +0900

    R3.10.27 Googleログインボタン、アプリケーション日本語化対応実装終了 Herokuアップ前

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module Photomemo
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja # デフォルトの言語を日本語に設定します。
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
